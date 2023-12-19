6a550a011ba42faa68b9c47ada35fd6db191e872
RizkySyakuur <rizkysyakuurrahman18@gmail.com>
2023-12-19 10:39:44 +0700

    commit

---
+++ src/Latihan/Biodataa.java
@@ -0,0 +1,12 @@
+package Latihan;
+
+import javax.swing.*;
+
+public class Biodataa {
+    private JPanel Panelmain;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField textField2;
+}
